a2a0a955393841ebb45db106ce1c4bf4df915831
moran.mishan <mishanmo@gmail.com>
2018-08-27 13:49:18 +0300

    changing default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#index', as: 'pages_index'
+
   resources :reviews
   resources :restaurants
-  get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
