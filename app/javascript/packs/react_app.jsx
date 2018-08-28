46e61fde92a2ddf141b101cd5e15ad4691501b20
moran.mishan <mishanmo@gmail.com>
2018-08-28 09:57:29 +0300

    integrating css modules  

--- app/javascript/packs/react_app.jsx
+++ app/javascript/packs/react_app.jsx
@@ -4,23 +4,12 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import AppContainer from './components/app/app_container'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello />,
+    <AppContainer />,
     document.body.appendChild(document.createElement('div')),
   )
 })
